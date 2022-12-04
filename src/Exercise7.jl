5600e43541f27fdb5bf8f1767cd1fc03139e2b57
Alistair White <alistair@pik-potsdam.de>
2022-12-04 11:12:13 +0100

    Rename and add dependencies

---
+++ src/Exercise7.jl
@@ -0,0 +1,8 @@
+module Exercise7 
+    export SIRV, plot
+
+    using DynamicalSystems, CairoMakie, Printf
+
+    include("models.jl")
+    include("plots.jl")
+end
--- src/SIRV.jl
+++
@@ -1,7 +0,0 @@
-module SIRV 
-    export f_SIRV
-
-    using OrdinaryDiffEq, CairoMakie
-
-    include("equations.jl")
-end
