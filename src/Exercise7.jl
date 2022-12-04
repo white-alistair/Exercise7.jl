47be972c1f282fff20b5869e0ca5b00db0d6d6c8
Alistair White <alistair@pik-potsdam.de>
2022-12-04 13:47:11 +0100

    Add plot of total infections vs vax rate

--- src/Exercise7.jl
+++ src/Exercise7.jl
@@ -1,5 +1,5 @@
 module Exercise7 
-    export SIR, SIRV, SIRVSeasonal, SIRVDecayingImmunity, plot, plot_vaccination_rate
+    export SIR, SIRV, SIRVSeasonal, SIRVDecayingImmunity, plot, plot_trajectory_given_vax_rate, plot_total_infections_by_vax_rate
 
     using DynamicalSystems, CairoMakie, Printf
 
--- src/plots.jl
+++ src/plots.jl
@@ -1,4 +1,4 @@
-function plot(model::AbstractSIRModel{T}; endtime::T = 365.0, Δt::T = 1.0, u0::Vector{T} = rand(T, 3)) where {T<:AbstractFloat}
+function plot(model::AbstractSIRModel{T}; endtime::T=365.0, Δt::T=1.0, u0::Vector{T}=rand(T, 3)) where {T<:AbstractFloat}
     u0 /= sum(u0)  # Normalise so that S + I + R = 1
     ds = ContinuousDynamicalSystem(model, u0, nothing)
     tr = trajectory(ds, endtime; Δt)
@@ -9,7 +9,7 @@ function plot(model::AbstractSIRModel{T}; endtime::T = 365.0, Δt::T = 1.0, u0::
 
     # Plotting
     fig = Figure(resolution=(1200, 600))
-    ax = Axis(fig[1, 1], xlabel="days", title= @sprintf "SIR Model with β = %.2f, γ = %.3f" model.β model.γ)
+    ax = Axis(fig[1, 1], xlabel="days", title=@sprintf "SIR Model with β = %.2f, γ = %.3f" model.β model.γ)
     times = Δt * collect(1:length(S))
     lines!(ax, times, S, linewidth=3, label="S")
     lines!(ax, times, I, linewidth=3, label="I")
@@ -18,7 +18,7 @@ function plot(model::AbstractSIRModel{T}; endtime::T = 365.0, Δt::T = 1.0, u0::
     return fig
 end
 
-function plot(model::AbstractSIRVModel{T}; endtime::T = 365.0, Δt::T = 1.0, u0::Vector{T} = rand(T, 4)) where {T<:AbstractFloat}
+function plot(model::AbstractSIRVModel{T}; endtime::T=365.0, Δt::T=1.0, u0::Vector{T}=rand(T, 4)) where {T<:AbstractFloat}
     u0 /= sum(u0)  # Normalise so that S + I + R + V = 1
     ds = ContinuousDynamicalSystem(model, u0, nothing)
     tr = trajectory(ds, endtime; Δt)
@@ -29,7 +29,7 @@ function plot(model::AbstractSIRVModel{T}; endtime::T = 365.0, Δt::T = 1.0, u0:
 
     # Plotting
     fig = Figure(resolution=(1200, 600))
-    ax = Axis(fig[1, 1], xlabel="days", title= @sprintf "SIRV Model with β = %.2f, γ = %.3f, ν = %.2f" model.β model.γ model.ν)
+    ax = Axis(fig[1, 1], xlabel="days", title=@sprintf "SIRV Model with β = %.2f, γ = %.3f, ν = %.2f" model.β model.γ model.ν)
     times = Δt * collect(1:length(S))
     lines!(ax, times, S, linewidth=3, label="S")
     lines!(ax, times, I, linewidth=3, label="I")
@@ -39,8 +39,25 @@ function plot(model::AbstractSIRVModel{T}; endtime::T = 365.0, Δt::T = 1.0, u0:
     return fig
 end
 
-function plot_vaccination_rate(ν::T; endtime::T = 365.0, Δt::T = 1.0, u0::Vector{T} = rand(T, 4)) where {T<:AbstractFloat}
+function plot_trajectory_given_vax_rate(ν::T; endtime::T=365.0, Δt::T=1.0, u0::Vector{T}=rand(T, 4)) where {T<:AbstractFloat}
     model = SIRV(ν)
     fig = plot(model; endtime, Δt, u0)
+    return fig
+end
+
+function plot_total_infections_by_vax_rate(ν_range::AbstractVector{T}; endtime::T=365.0, Δt::T=1.0, u0::Vector{T}=rand(T, 4)) where {T<:AbstractFloat}
+    total_infections = T[]
+
+    for ν in ν_range
+        model = SIRV(ν)
+        ds = ContinuousDynamicalSystem(model, u0, nothing)
+        tr = trajectory(ds, endtime; Δt)
+        R_end = tr[end][3]
+        push!(total_infections, R_end)
+    end
+
+    fig = Figure()
+    ax = Axis(fig[1, 1], xlabel = "Vaccination Rate", ylabel = "Total Infections")
+    scatter!(ax, ν_range, total_infections)
     return fig
 end
