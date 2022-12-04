70061d600b405e7c5f031178b82b7dffdfe71d9f
Alistair White <alistair@pik-potsdam.de>
2022-12-04 11:12:36 +0100

    Replace equations with models

--- src/models.jl
+++ src/models.jl
@@ -1,11 +1,17 @@
-function f_SIRV(du, u, p, t)
+struct SIRV{T<:AbstractFloat}
+    β::T
+    γ::T
+    ν::T
+end
+
+function (model::SIRV)(du, u, p, t)
     S, I, R, V = u
-    β, γ, ν = p
+    (; β, γ, ν) = model
         
     du[1] = -β * S * I - ν * S
     du[2] = β * S * I - γ * I
     du[3] = γ * I
     du[4] = ν * S
-    
+
     return nothing
 end
