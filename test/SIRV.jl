90ff47ae4b9abfe4fbbf03f080aac8fc0e67f1ee
Alistair White <alistair@pik-potsdam.de>
2022-12-05 11:00:10 +0100

    Update tests

--- test/SIRV.jl
+++ test/SIRV.jl
@@ -1,45 +1,5 @@
-@testset "models.jl" begin
-    @testset "SIR Model" begin
-        # Constructors
-        β, γ = 1.0, 2.0
-        model = SIR(β, γ)
-        @test model.β == β
-        @test model.γ == γ
-
-        # Defaults
-        model = SIR()
-        @test model.β == 0.35
-        @test model.γ == 0.035
-
-        # Equations
-        du = zeros(3)
-        u = [1.0, 2.0, 3.0]
-        model(du, u, nothing, nothing)
-        @test du ≈ [-0.7, 0.63, 0.07]
-    end
-
-    @testset "SIR Model with Decaying Immunity" begin
-        # Constructors
-        β, γ, α = 1.0, 2.0, 3.0
-        model = SIRDecayingImmunity(β, γ, α)
-        @test model.β == β
-        @test model.γ == γ
-        @test model.α == α
-
-        # Defaults
-        model = SIRDecayingImmunity(α)
-        @test model.β == 0.35
-        @test model.γ == 0.035
-        @test model.α == α
-
-        # Equations
-        du = zeros(3)
-        u = [1.0, 2.0, 3.0]
-        model(du, u, nothing, nothing)
-        @test du ≈ [8.3, 0.63, -8.93]
-    end
-
-    @testset "SIRV Model" begin
+@testset "SIRV.jl" begin
+    @testset "Standard SIRV Model" begin
         # Constructors
         β, γ, ν = 1.0, 2.0, 3.0
         model = SIRV(β, γ, ν)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using Exercise7
 using Test
 
 @testset "Exercise7.jl" begin
-    include("models.jl")
+    include("SIR.jl")
+    include("SIRV.jl")
 end
